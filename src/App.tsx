4b4b2ebcb11444091441a3765803aa2dbf61f888
Islam <rustamov.exceedteam@gmail.com>
2020-03-13 17:07:37 +0300

    Implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
-
-import TodoForm from './components/TodoForm/TodoForm';
-import TodoLogin from './components/TodoLogin/TodoLogin';
-import TodoSignup from './components/TodoSignup/TodoSignup';
-
-//import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-
-// import AuthenticationApi from './components/api/AuthenticationApi';
-// import TodoApi from './components/api/todoApi';
-// import ApiRequests from '../src/components/api/ApiRequests';
-
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes/routes';
+import SecuredRoutes from './routes/securedRoutes';
 import './App.css';
-
-// const apiReqs = new ApiRequests();
-
-const appURL = 'https://mysterious-savannah-44011.herokuapp.com/';
-
-// const getBody = {
-//   method: 'GET',
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-// };
-
-// const user = new AuthenticationApi(apiReqs);
-// const todos = new TodoApi(apiReqs, user);
 
 function App() {
   return (
     <div className="todo-interface">
       <h1>todos</h1>
-      <TodoLogin />
-      <h6>Double-click to edit a todo</h6>
+      <BrowserRouter>
+        {localStorage.getItem('token') ? <SecuredRoutes /> : <Routes />}
+      </BrowserRouter>
     </div>
   );
 }
